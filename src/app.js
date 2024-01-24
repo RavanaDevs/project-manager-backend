9424f0a43f00b2909286f43ba3645ea81fd210dc
Kavindu Sanjula <ravanadevs@gmail.com>
2024-01-24 12:27:51 +0530

    Create projects structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import homeRouter from './routes/home.js';
+import projectRouter from './routes/project.js';
 import authRouter from './auth/routes/auth-routes.js';
 
 const app = express();
@@ -9,5 +10,6 @@ app.use(express.json());
 
 app.use('/', homeRouter);
 app.use('/auth', authRouter);
+app.use('/projects', projectRouter);
 
 export default app;
---
+++ src/routes/project.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import checkAuth from '../middlewares/checkAuth.js'
+
+
+const router = express.Router();
+
+
+export default router;
